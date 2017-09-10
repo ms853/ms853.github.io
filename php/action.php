a0cae1a5161835f0b2a600123482cca81886e387
Mike <ms853@le.ac.uk>
2017-09-10 02:10:44 +0100

    update on my website - add a incomplete skills section

--- php/action.php
+++ php/action.php
@@ -1,12 +1,19 @@
+<html>
+<head>
+<title>Thank You!</title>
+</head>
+<body>
 <?php 
 /*PHP code for the form handling*/
 
 $name = $_POST['name'];
 $email = $_POST['email'];
-$message = $_POST['comment'];
-$to = "msakyiama@outlook.com";
+$message = $_POST['message'];
+$to = "msakyiama51@gmail.com";
 $subject = "New Message";
 
 mail($to, $subject, $message, "From: " . $name);
-echo "Thank you for your message, your message has been sent!"
+echo "Thank you for your message! I will get back to you!"
 ?>
+</body>
+</html>
